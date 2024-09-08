142a3c317689510f866b529e029ca2c0ff894398
MattRT21 <matt.d.gonzalez05@gmail.com>
2024-09-08 05:36:02 +0000

    Prologue

---
+++ fracturing.c
@@ -0,0 +1,13 @@
+//*************************************************************************
+// Assignment 1
+// fracturing.c
+// Author: Matthew Gonzalez
+// UCFiD: 5637788
+// Date: 9/7/24
+// Class: COP 3223, Professor Parra
+// Purpose: Utilize multiple internal functions, experience fracturing code
+// Input: 
+// Output:  
+//*************************************************************************
+
+
